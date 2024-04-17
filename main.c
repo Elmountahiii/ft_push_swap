29415aadaa6ab452bf9f88a2c1525d07f5737d6f
Youssef ElMountahi <yusuf.one@outlook.com>
2024-04-17 13:35:53 +0100

    code  refactoring

--- main.c
+++ main.c
@@ -1,20 +1,12 @@
 #include "./mandatory/push_swap.h"
 
-void print_stack_content(t_stack *stack)
-{
-    while (stack)
-    {
-        printf("%d : %d\n",stack->index,stack->value);
-        stack = stack->next;
-    }
-}
 int main(int argc, char **argv)
 {
     t_stack *a;
-   t_stack *b;
+    t_stack *b;
 
     a = NULL;
-   b = NULL;
+    b = NULL;
     if (argc < 2)
         return 0;
     ft_init_stack(&a, argc, argv);
@@ -23,6 +15,5 @@ int main(int argc, char **argv)
     if (ft_is_sorted(a))
         return (0);
     ft_push_swap(&a, &b);
-   // print_stack_content(a);
     return 0;
 }
--- mandatory/stackUtils/ft_get_stack_last.c
+++
@@ -1,8 +0,0 @@
-#include "../push_swap.h"
-
-int ft_get_stack_last(t_stack *stack)
-{
-    while (stack->next)
-        stack = stack->next;
-    return (stack->value);
-}
